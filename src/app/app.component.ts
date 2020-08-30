6e91ab96465bdaa8d5a21d7c0befb74c1d4ab79d
Khanh <knguyen2@stevens.edu>
2020-08-29 22:28:15 -0400

    UI Buttons and Search bar
    updated UI code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,53 @@
-﻿import { Component } from '@angular/core';
+﻿import {Component, ViewChild} from '@angular/core';
 
 import { AccountService } from './_services';
 import { User } from './_models';
+import { Papa} from 'ngx-papaparse';
+import { NgxSpinnerService} from 'ngx-spinner';
+import { AgGridAngular} from 'ag-grid-angular';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
     user: User;
+    private csvRecords: any[];
+    @ViewChild( 'agGrid') agGrid: AgGridAngular;
 
-    constructor(private accountService: AccountService) {
+    constructor(private accountService: AccountService, private papa: Papa, private spinner: NgxSpinnerService) {
         this.accountService.user.subscribe(x => this.user = x);
+        const csvData = '"Hello","World!"';
+        this.papa.parse(csvData,{
+        complete: (result) => {
+          console.log('Parsed: ', result);
+        }});
     }
 
     logout() {
         this.accountService.logout();
     }
+  handleDropedFile(evt) {
+    this.csvRecords = [];
+    this.spinner.show();
+    const files = evt.target.files;  // File List object
+    const file = files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (event: any) => {
+      const csv = event.target.result; // Content of CSV file
+      this.papa.parse(csv, {
+        skipEmptyLines: true,
+        header: true,
+        complete: results => {
+          const data = results.data;
+          this.csvRecords = data;
+          console.log(this.csvRecords);
+          const total = this.csvRecords.length;
+          if (total == 0) {
+            this.spinner.hide();
+            alert('no data in csv');
+            return;
+          }
+        }
+      })
+    }
+  }
 }
